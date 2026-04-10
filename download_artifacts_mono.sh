28a70c6b10e8bbe0148f7f5bdd2a7d4c69e39f69
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2026-04-10 13:40:31 +0530

    Improved download scripts

--- download_artifacts_mono.sh
+++ download_artifacts_mono.sh
@@ -5,8 +5,6 @@ mkdir -p artifacts/downloads
 rm -rf artifacts/mono
 mkdir -p artifacts/mono
 
-cd artifacts/downloads
-
 : "${GODOT_VERSION:=4.6.1}"
 : "${RELEASE_NAME:=stable}"
 
@@ -14,12 +12,10 @@ export GODOT_VERSION_STUB="${GODOT_VERSION}-${RELEASE_NAME}"
 
 echo $GODOT_VERSION_STUB
 
-wget -q -O mono_linux_x86_64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_linux_x86_64.zip&platform=linux.64" \
-& wget -q -O mono_linux_arm64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_linux_arm64.zip&platform=linux.arm64" \
-& wget -q -O mono_export_templates.tpz "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_export_templates.tpz&platform=templates" \
+wget -q -O artifacts/downloads/mono_linux_x86_64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_linux_x86_64.zip&platform=linux.64" \
+& wget -q -O artifacts/downloads/mono_linux_arm64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_linux_arm64.zip&platform=linux.arm64" \
+& wget -q -O artifacts/downloads/mono_export_templates.tpz "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=mono_export_templates.tpz&platform=templates" \
 & wait
-
-cd ../../
 
 unzip artifacts/downloads/mono_linux_arm64.zip -d artifacts/mono/ \
 && unzip artifacts/downloads/mono_linux_x86_64.zip -d artifacts/mono/ \
--- download_artifacts_standard.sh
+++ download_artifacts_standard.sh
@@ -5,8 +5,6 @@ mkdir -p artifacts/downloads
 rm -rf artifacts/standard
 mkdir -p artifacts/standard
 
-cd artifacts/downloads
-
 : "${GODOT_VERSION:=4.6.1}"
 : "${RELEASE_NAME:=stable}"
 
@@ -14,12 +12,10 @@ export GODOT_VERSION_STUB="${GODOT_VERSION}-${RELEASE_NAME}"
 
 echo $GODOT_VERSION_STUB
 
-wget -q -O linux.x86_64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=linux.x86_64.zip&platform=linux.64" \
-& wget -q -O linux.arm64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=linux.arm64.zip&platform=linux.arm64" \
-& wget -q -O export_templates.tpz "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=export_templates.tpz&platform=templates" \
+wget -q -O artifacts/downloads/linux.x86_64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=linux.x86_64.zip&platform=linux.64" \
+& wget -q -O artifacts/downloads/linux.arm64.zip "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=linux.arm64.zip&platform=linux.arm64" \
+& wget -q -O artifacts/downloads/export_templates.tpz "https://downloads.godotengine.org/?version=${GODOT_VERSION}&flavor=${RELEASE_NAME}&slug=export_templates.tpz&platform=templates" \
 & wait
-
-cd ../../
 
 unzip artifacts/downloads/linux.arm64.zip -d artifacts/standard/ \
 && unzip artifacts/downloads/linux.x86_64.zip -d artifacts/standard/ \
